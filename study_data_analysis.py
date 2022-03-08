77661384313228ac8d4422e7bb9f9d7f472f2837
Mathew Alphons <mathewalphons1982@gmail.com>
2022-03-08 20:45:48 +0400

    Data Analysis files 1 commit

---
+++ study_data_analysis.py
@@ -0,0 +1,123 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 1 - NUMPY ARRAYS##########
+
+# First 20 countries with employment data
+countries = np.array([
+    'Afghanistan', 'Albania', 'Algeria', 'Angola', 'Argentina',
+    'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas',
+    'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium',
+    'Belize', 'Benin', 'Bhutan', 'Bolivia',
+    'Bosnia and Herzegovina'
+])
+
+# Employment data in 2007 for those 20 countries
+employment = np.array([
+    55.70000076,  51.40000153,  50.5       ,  75.69999695,
+    58.40000153,  40.09999847,  61.5       ,  57.09999847,
+    60.90000153,  66.59999847,  60.40000153,  68.09999847,
+    66.90000153,  53.40000153,  48.59999847,  56.79999924,
+    71.59999847,  58.40000153,  70.40000153,  41.20000076
+])
+
+def max_employment(countries, employment):
+    '''
+    Fill in this function to return the name of the country
+    with the highest employment in the given employment
+    data, and the employment in that country.
+    '''
+    max_ind = employment.argmax()
+    max_country = countries[max_ind]
+    max_value = employment[max_ind]
+    
+    return (max_country, max_value)
+    
+max_country, max_value = max_employment(countries, employment)
+print("Maximum Employment Countries", "/n")
+print(max_country, max_value)
+
+# 2- Calculate Overall Completion Rate#######
+
+# First 20 countries with school completion data
+countries = np.array([
+       'Algeria', 'Argentina', 'Armenia', 'Aruba', 'Austria','Azerbaijan',
+       'Bahamas', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Bolivia',
+       'Botswana', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi',
+       'Cambodia', 'Cameroon', 'Cape Verde'
+])
+
+# Female school completion rate in 2007 for those 20 countries
+female_completion = np.array([
+    97.35583,  104.62379,  103.02998,   95.14321,  103.69019,
+    98.49185,  100.88828,   95.43974,   92.11484,   91.54804,
+    95.98029,   98.22902,   96.12179,  119.28105,   97.84627,
+    29.07386,   38.41644,   90.70509,   51.7478 ,   95.45072
+])
+
+# Male school completion rate in 2007 for those 20 countries
+male_completion = np.array([
+     95.47622,  100.66476,   99.7926 ,   91.48936,  103.22096,
+     97.80458,  103.81398,   88.11736,   93.55611,   87.76347,
+    102.45714,   98.73953,   92.22388,  115.3892 ,   98.70502,
+     37.00692,   45.39401,   91.22084,   62.42028,   90.66958
+])
+
+def overall_completion_rate(female_completion, male_completion):
+    '''
+    Fill in this function to return a NumPy array containing the overall
+    school completion rate for each country. The arguments are NumPy
+    arrays giving the female and male completion of each country in
+    the same order.
+    '''
+    return (female_completion+male_completion)/2.0
+
+print("Overall Completion Rate", "/n")
+overall_completion_rate = overall_completion_rate(female_completion, male_completion)
+for i in range(countries.size):
+    print(countries[i], overall_completion_rate[i])
+
+
+#Pandas Series apply()
+
+names = pd.Series([
+    'Andre Agassi',
+    'Barry Bonds',
+    'Christopher Columbus',
+    'Daniel Defoe',
+    'Emilio Estevez',
+    'Fred Flintstone',
+    'Greta Garbo',
+    'Humbert Humbert',
+    'Ivan Ilych',
+    'James Joyce',
+    'Keira Knightley',
+    'Lois Lane',
+    'Mike Myers',
+    'Nick Nolte',
+    'Ozzy Osbourne',
+    'Pablo Picasso',
+    'Quirinus Quirrell',
+    'Rachael Ray',
+    'Susan Sarandon',
+    'Tina Turner',
+    'Ugueth Urbina',
+    'Vince Vaughn',
+    'Woodrow Wilson',
+    'Yoji Yamada',
+    'Zinedine Zidane France'
+])
+
+def reverse_names(names):
+    '''
+    Fill in this function to return a new series where each name
+    in the input series has been transformed from the format
+    "Firstname Lastname" to "Lastname, FirstName".
+    
+    Try to use the Pandas apply() function rather than a loop.
+    '''
+    return names.apply(lambda x: ", ".join(x.split()[::-1]))
+print("Reverse Name", "/n")
+print(reverse_names(names))
